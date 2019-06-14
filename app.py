2e15b40b4dec9e783948a03582721ad2dd735b25
Ubuntu <ubuntu@ip-172-31-40-27.ec2.internal>
2019-06-14 20:03:09 +0000

    added more templates / picture(just one for now and cards have been hardcoded to it)/ added filter for aperitifs search

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app=Flask(__name__)
@@ -7,13 +9,26 @@ app=Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cook-book'
 app.config["MONGO_URI"] = 'mongodb+srv://tode:1Martie1998@myfirstcluster-dxyoc.mongodb.net/cook-book?retryWrites=true&w=majority'
 
-
+mongo = PyMongo(app)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+    
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html",
+             recipes=mongo.db.recipes.find())
+             
+             
+@app.route("/aperitif")
+def aperitif():
+    return render_template("aperitif.html",
+    recipes=mongo.db.recipes.find({
+            "$and": [{"dish_category": 'Main'},
+                    {"dish_category": {'$exists': True}}]}))
 
 
 
