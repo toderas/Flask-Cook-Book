0ff791a8a3834c43c999b00bfbfdb728cb7b4054
Ubuntu <ubuntu@ip-172-31-40-27.ec2.internal>
2019-06-14 01:34:50 +0000

    changed port settings in cookbook.py file

--- cookbook.py
+++ cookbook.py
@@ -17,5 +17,11 @@ def hello():
     return "Hello World!"
 
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+
+#if __name__ == '__main__':
+  #  app.run(host='0.0.0.0', port=8080, debug=True)
