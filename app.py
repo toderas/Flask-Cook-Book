c162688b3b54cabde98841402e681c2aaec6fb69
Ubuntu <ubuntu@ip-172-31-40-27.ec2.internal>
2019-06-14 03:14:20 +0000

    cookbook.py renamed to app.py / added style folder / added main page navigation

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+
 
 app=Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cook-book'
 app.config["MONGO_URI"] = 'mongodb+srv://tode:1Martie1998@myfirstcluster-dxyoc.mongodb.net/cook-book?retryWrites=true&w=majority'
 
-mongo = PyMongo(app)
 
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+     app.run(host='0.0.0.0', port=8080, debug=True)
 
-#if __name__ == '__main__':
-  #  app.run(host='0.0.0.0', port=8080, debug=True)
