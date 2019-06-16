c3685242d0fd2df3a5b60dcaf6045eb14bff0188
Ubuntu <ubuntu@ip-172-31-40-27.ec2.internal>
2019-06-16 07:16:21 +0000

    deleted unnecesary html files / added functionality for displaying one recipe / added routes for all categories(starter, main,,,etc)

--- app.py
+++ app.py
@@ -25,31 +25,64 @@ def recipes():
              
 @app.route("/aperitif")
 def aperitif():
-    return render_template("aperitif.html",
+    return render_template("recipes.html",
     recipes=mongo.db.recipes.find({
-            "$and": [{"dish_category": 'Aperitif'},
-                    {"dish_category": {'$exists': True}}]}))
+            "$and": [{"category_name": 'Aperitif'},
+                    {"category_name": {'$exists': True}}]}))
                     
 @app.route("/starter")
 def starter():
-    return render_template("starter.html",
+    return render_template("recipes.html",
     recipes=mongo.db.recipes.find({
-            "$and": [{"dish_category": 'Starter'},
-                    {"dish_category": {'$exists': True}}]}))
+            "$and": [{"category_name": 'Starter'},
+                    {"category_name": {'$exists': True}}]}))
+                    
+@app.route("/intermediate")
+def intermediate():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find({
+            "$and": [{"category_name": 'Intermediate'},
+                    {"category_name": {'$exists': True}}]}))
+                    
+@app.route("/main")
+def main():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find({
+            "$and": [{"category_name": 'Main'},
+                    {"category_name": {'$exists': True}}]}))
+                    
+@app.route("/dessert")
+def dessert():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find({
+            "$and": [{"category_name": 'Dessert'},
+                    {"category_name": {'$exists': True}}]}))
 
 
 
-@app.route("/addreceipe")
-def addreceipe():
-    return render_template("addreceipe.html")
+@app.route("/addrecipe")
+def addrecipe():
+    return render_template("addrecipe.html",
+                           categories=mongo.db.categories.find(),
+                           skills=mongo.db.skills.find())
     
 
-@app.route('/add_recipe', methods=['POST'])
-def add_recipe():
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
     recipes =  mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('recipes'))
-    flash('Recipe has been added', 'success')
+    
+    
+@app.route('/show_recipe/<recipe_id>')
+def show_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_recipes =  mongo.db.recipes.find(the_recipe)
+    return render_template('showrecipe.html', recipe=the_recipe,
+                           recipes=all_recipes)
+    
+    
+
 
 
 if __name__ == '__main__':
