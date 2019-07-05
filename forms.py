6483a7b33d7c7f85db6caa5e96b339a3e49e8936
Toader <toaderchirila21@gmail.com>
2019-07-05 16:07:11 +0000

    added form validation criteria

--- forms.py
+++ forms.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, IntegerField, SubmitField, FloatField
+from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class AddRecipeForm(FlaskForm):
+    dish_name = StringField('Name',
+                            validators=[DataRequired(), Length(min=3, max=50)])
     dish_author = StringField('Author',
-                           validators=[DataRequired(), Length(min=10, max=20)])
-    dish_prep_time = IntegerField('Prep Time',
-                        validators=[DataRequired(), Length(min=1, max=3)])
-    
+                           validators=[DataRequired(), Length(min=4, max=20)])
+    dish_origin_cuisine = StringField('Country Of Origin',
+                                      validators=[DataRequired(), Length(min=2, max=10)])
+    dish_prep_time = IntegerField('Estimated Preparation time (in minutes)',
+                                  validators=[DataRequired(), NumberRange(0,999)])
+    dish_ingredients = StringField('Ingredients',
+                                      validators=[DataRequired(), Length(min=2, max=200)])
+    dish_preparation_steps = StringField('Recommended Steps',
+                                      validators=[DataRequired(), Length(min=2, max=200)])
     submit = SubmitField('Add Recipe')
