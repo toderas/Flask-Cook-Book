849275574280479897d38809e4bb7a1c2fc3f05f
Toader <toaderchirila21@gmail.com>
2019-07-06 12:30:50 +0000

    validation in place for editrecipe

--- forms.py
+++ forms.py
@@ -17,3 +17,18 @@ class AddRecipeForm(FlaskForm):
     dish_preparation_steps = StringField('Recommended Steps',
                                       validators=[DataRequired(), Length(min=2, max=200)])
     submit = SubmitField('Add Recipe')
+    
+class EditRecipeForm(FlaskForm):
+    dish_name = StringField('Name',
+                            validators=[DataRequired(), Length(min=3, max=50)])
+    dish_author = StringField('Author',
+                           validators=[DataRequired(), Length(min=4, max=20)])
+    dish_origin_cuisine = StringField('Country Of Origin',
+                                      validators=[DataRequired(), Length(min=2, max=10)])
+    dish_prep_time = IntegerField('Estimated Preparation time (in minutes)',
+                                  validators=[DataRequired(), NumberRange(0,999)])
+    dish_ingredients = StringField('Ingredients',
+                                      validators=[DataRequired(), Length(min=2, max=200)])
+    dish_preparation_steps = StringField('Recommended Steps',
+                                      validators=[DataRequired(), Length(min=2, max=200)])
+    submit = SubmitField('Update Recipe')
