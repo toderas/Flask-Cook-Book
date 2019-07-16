1f086f4b7c133ded118dbfd9f064e070d5b7410b
Toader <toaderchirila21@gmail.com>
2019-07-16 21:02:30 +0000

    pep8 validation (the code passes with errors due to length but as github code review is 120 characters this should not cause any issues)

--- helpers.py
+++ helpers.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, flash
 from flask_pymongo import PyMongo, pymongo
 import csv
-import pprint;
 
 
 app = Flask(__name__)
-# path for csv file 
+# path for csv file
 data_file = "static/data/recipe.csv"
+
 
 def get_page():
     return request.args.get('page', 1, type=int)
 
-# pagination 
+
+# pagination
 def paginate_list(query, page_number, per_page):
     array = [item for item in query]
     paginated_array = array[((page_number*per_page)-per_page):(page_number*per_page)]
     return paginated_array
 
 
-# writes data on csv file 
+# writes data on csv file
 def write_to_csv(data_file, cursor):
     with open(data_file, "w+") as outfile:
-        fields = [ "dish_name", "dish_author","dish_views","dish_upvotes","dish_prep_time","dish_required_skill", "dish_origin_cuisine", "category_name"]
+        fields = ["dish_name", "dish_author", "dish_views", "dish_upvotes",
+                  "dish_prep_time", "dish_required_skill", "dish_origin_cuisine", "category_name"]
         writer = csv.DictWriter(outfile, fieldnames=fields)
         writer.writeheader()
         for x in cursor:
             writer.writerow(x)
-            
